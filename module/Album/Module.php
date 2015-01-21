f01d0a0bb8343f72ea606d4c500707e33a566a1d
Troy Connor <troy.connor@yahoo.com>
2015-01-20 19:56:00 -0500

    fixed indent errors.

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -14,20 +14,19 @@ use Zend\Mvc\MvcEvent;
 
 class Module
 {
-  public function onBootstrap(MvcEvent $e)
-  {
-    $eventManager        = $e->getApplication()->getEventManager();
-    $moduleRouteListener = new ModuleRouteListener();
-    $moduleRouteListener->attach($eventManager);
-  }
+    public function onBootstrap(MvcEvent $e)
+    {
+      $eventManager        = $e->getApplication()->getEventManager();
+      $moduleRouteListener = new ModuleRouteListener();
+      $moduleRouteListener->attach($eventManager);
+    }
 
-  public function getConfig()
-  {
-    return include __DIR__ . '/config/module.config.php';
-  }
+    public function getConfig()
+    {
+      return include __DIR__ . '/config/module.config.php';
+    }
 
-  public function getAutoloaderConfig()
-  {
-  }
-
+    public function getAutoloaderConfig()
+    {
+    }
 }
